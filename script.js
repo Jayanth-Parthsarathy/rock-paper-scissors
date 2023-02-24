8a6cb96c6c5ea70411c2e61048b81cc682c23eb9
jayanth-parthsarathy <jparthaa@gmail.com>
2023-02-24 19:51:35 +0530

    Add UI

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const rock = document.getElementById("rock");
+const paper = document.getElementById("paper");
+const scissors = document.getElementById("scissors");
+const result = document.getElementById("result");
+const reset = document.getElementById("reset");
+let noOfTurns = 1;
+const scores = document.getElementById("scores");
+let userScore = 0;
+let computerScore = 0;
 function toTitleCase(str) {
   return str.replace(/\w\S*/g, function (txt) {
     return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
@@ -11,6 +20,15 @@ function getComputerChoice() {
 }
 
 function playRound(playerSelection, computerSelection) {
+  if (noOfTurns == 5 && computerScore == userScore) {
+    result.textContent = `You are tied with the Computer`;
+  } else if (computerScore == 5) {
+    result.textContent = `You lost! The computer won!`;
+    return;
+  } else if (userScore == 5) {
+    result.textContent = `You are the winner!!`;
+    return;
+  }
   playerSelection = toTitleCase(playerSelection);
   computerSelection = toTitleCase(computerSelection);
   let won = false;
@@ -26,37 +44,67 @@ function playRound(playerSelection, computerSelection) {
     won = true;
   }
   if (won) {
-    return `${playerSelection} beats ${computerSelection}. You Won!`;
+    userScore += 1;
+    scores.textContent = `Player Score:${userScore}  Computer Score:${computerScore}`;
+    result.textContent = `${playerSelection} beats ${computerSelection}. You Won!`;
   } else if (draw == true) {
-    return `You both chose ${playerSelection}. It's a Tie!`;
+    result.textContent = `You both chose ${playerSelection}. It's a Tie!`;
+    scores.textContent = `Player Score:${userScore}  Computer Score:${computerScore}`;
   } else {
-    return `${computerSelection} beats ${playerSelection}. You Lose!`;
+    computerScore += 1;
+    result.textContent = `${computerSelection} beats ${playerSelection}. You Lose!`;
+    scores.textContent = `Player Score:${userScore}  Computer Score:${computerScore}`;
   }
 }
 
-function game() {
-  let playerScore = 0;
-  let computerScore = 0;
-  for (let i = 0; i < 5; i++) {
-    let playerSelection = prompt("Enter your choice: ");
-    let computerSelection = getComputerChoice();
-    let curResult = playRound(playerSelection, computerSelection);
-    let temp = curResult.slice(-3);
-    if (temp == "se!") {
-      computerScore++;
-    } else if (temp == "on!") {
-      playerScore++;
-    }
-    console.log(curResult);
-  }
-  console.log(playerScore);
-  console.log(computerScore);
-  if (playerScore > computerScore) {
-    console.log("You are the winner!!");
-  } else if (playerScore == computerScore) {
-    console.log("You are tied with the Computer");
-  } else {
-    console.log("You lost! The computer won!");
-  }
-}
-game();
+// function game() {
+//   let playerScore = 0;
+//   let computerScore = 0;
+//   for (let i = 0; i < 5; i++) {
+//     let playerSelection = prompt("Enter your choice: ");
+//     let computerSelection = getComputerChoice();
+//     let curResult = playRound(playerSelection, computerSelection);
+//     let temp = curResult.slice(-3);
+//     if (temp == "se!") {
+//       computerScore++;
+//     } else if (temp == "on!") {
+//       playerScore++;
+//     }
+//     console.log(curResult);
+//   }
+//   console.log(playerScore);
+//   console.log(computerScore);
+//   if (playerScore > computerScore) {
+//     console.log("You are the winner!!");
+//   } else if (playerScore == computerScore) {
+//     console.log("You are tied with the Computer");
+//   } else {
+//     console.log("You lost! The computer won!");
+//   }
+// }
+// game();
+
+rock.addEventListener("click", function () {
+  playRound("rock", getComputerChoice());
+});
+paper.addEventListener("click", function () {
+  playRound("paper", getComputerChoice());
+});
+scissors.addEventListener("click", function () {
+  playRound("scissors", getComputerChoice());
+});
+
+reset.addEventListener("click", () => {
+  userScore = 0;
+  computerScore = 0;
+  result.textContent = "";
+  scores.textContent = "";
+});
+
+const btn = document.querySelector(".btn-toggle");
+
+// Listen for a click on the button
+btn.addEventListener("click", function () {
+  // Then toggle (add/remove) the .dark-theme class to the body
+  document.body.classList.toggle("dark-theme");
+});
